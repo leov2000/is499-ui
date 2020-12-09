a15270e9c6a11ecba507671629fff20a9113c132
Leo Vergara <leovergarah@gmail.com>
2020-12-08 22:51:40 -0500

    (Added) basic Landing

---
+++ src/App/components/Landing/container/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Grid } from "../../Grid";
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+
+function Landing() {
+  return (
+    <div>
+      Landing Scaffolding...
+      <Grid/>
+    </div>
+  );
+}
+
+export default Landing;
---
+++ src/App/components/Landing/index.js
@@ -0,0 +1,5 @@
+import Landing from "./container"
+
+export {
+  Landing
+};
