87160909b724ffe3386ad7faf7584674f8b2619e
Leo Vergara <leovergarah@gmail.com>
2020-12-08 22:54:18 -0500

    (Added) basic Container

---
+++ src/App/container/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Landing } from "../components/Landing";
+import "../assets/style.less";
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+
+function App()  {
+  return (
+    <Landing/>
+  )
+}
+
+export default App;
