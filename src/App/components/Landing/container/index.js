971b8f944588afebf5280017d756b30c57331172
Leo Vergara <leovergarah@gmail.com>
2020-12-08 22:56:19 -0500

    (Updated) ag-grid css assets

--- src/App/components/Landing/container/index.js
+++ src/App/components/Landing/container/index.js
@@ -1,8 +1,5 @@
 import React from "react";
 import { Grid } from "../../Grid";
-import { AgGridReact } from 'ag-grid-react';
-import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 function Landing() {
   return (
