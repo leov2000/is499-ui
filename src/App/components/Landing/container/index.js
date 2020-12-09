c875019f791945fb5df0048da5424929463000a7
Leo Vergara <leovergarah@gmail.com>
2020-12-09 17:59:46 -0500

    (Added) data-wrapper chart implementation

--- src/App/components/Landing/container/index.js
+++ src/App/components/Landing/container/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { Grid } from "../../Grid";
+import DWChart from "react-datawrapper-chart";
 
 function Landing() {
   return (
     <div>
       Landing Scaffolding...
+      <div>
+        <DWChart title="Map" src="//datawrapper.dwcdn.net/7e1Ry/1/"/>
+      </div>
+      <div>
+        <DWChart title="Map"src="//datawrapper.dwcdn.net/5hALS/1/"/>
+      </div>
       <Grid/>
     </div>
   );
