1b0c098639be633b0370894a60acff14e71d3151
Leo Vergara <leovergarah@gmail.com>
2020-12-09 22:57:49 -0500

    (Added) Footer component

---
+++ src/App/components/Footer/container/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../assets/style.less";
+
+function Footer() {
+  return (
+    <div className="footer">
+      [CUNY SPS FALL 2020 IS-499 DR. CHARLES PAK] 
+    </div>
+  )
+}
+
+export default Footer;
