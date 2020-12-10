a6b54864ced8f094687b8bcc011a9b1cedda63f6
Leo Vergara <leovergarah@gmail.com>
2020-12-09 22:59:22 -0500

    (Updated) config and moved to outer dir

---
+++ src/App/config/index.js
@@ -0,0 +1,157 @@
+export const bullyingColumns = [
+  {
+    headerName: "ID",
+    field: "id"
+  },
+  {
+    headerName: "SCHOOL DBN",
+    field: "dbn"
+  },
+  {
+    headerName: "SCHOOL NAME",
+    field: "school_name"
+  },
+  {
+    headerName: "COMPLAINTS",
+    field: "complaints",
+    filter: 'agNumberColumnFilter'
+  },
+  {
+    headerName: "MATERIAL INCIDENTS",
+    field: "material_incidents",
+    filter: 'agNumberColumnFilter'
+  }
+];
+
+export const absentColumns = [
+  {
+    headerName: "ID",
+    field: "id"
+  },
+  {
+    headerName: "SCHOOL DBN",
+    field: "dbn"
+  },
+  {
+    headerName: "SCHOOL NAME",
+    field: "school_name"
+  },
+  {
+    headerName: "GRADE",
+    field: "grade"
+  },
+  {
+    headerName: "YEAR",
+    field: "year"
+  },
+  {
+    headerName: "DEMOGRAPHIC CATEGORY",
+    field: "demographic_category"
+  },
+  {
+    headerName: "DEMOGRAPHIC VARIABLE",
+    field: "demographic_variable"
+  },
+  {
+    headerName: "NUM TOTAL DAYS",
+    field: "num_total_days"
+  },
+  {
+    headerName: "NUM DAYS ABSENT",
+    field: "num_days_absent"
+  },
+  {
+    headerName: "NUM DAYS PRESENT",
+    field: "num_days_present"
+  },
+  {
+    headerName: "PERCENT ATTENDANCE",
+    field: "percent_attendance"
+  },
+  {
+    headerName: "NUM CHRONICALLY ABSENT",
+    field: "num_chronically_absent"
+  },
+  {
+    headerName: "PERCENT CHRONICALLY ABSENT",
+    field: "percent_chronically_absent"
+  },
+
+]
+
+
+export const allColumns = [
+  {
+    headerName: "ID",
+    field: "id"
+  },
+  {
+    headerName: "SCHOOL DBN",
+    field: "dbn"
+  },
+  {
+    headerName: "SCHOOL NAME",
+    field: "school_name"
+  },
+  {
+    headerName: "GRADE",
+    field: "grade"
+  },
+  {
+    headerName: "YEAR",
+    field: "year"
+  },
+  {
+    headerName: "DEMOGRAPHIC CATEGORY",
+    field: "demographic_category"
+  },
+  {
+    headerName: "DEMOGRAPHIC VARIABLE",
+    field: "demographic_variable"
+  },
+  {
+    headerName: "NUM TOTAL DAYS",
+    field: "num_total_days"
+  },
+  {
+    headerName: "NUM DAYS ABSENT",
+    field: "num_days_absent"
+  },
+  {
+    headerName: "NUM DAYS PRESENT",
+    field: "num_days_present"
+  },
+  {
+    headerName: "PERCENT ATTENDANCE",
+    field: "percent_attendance"
+  },
+  {
+    headerName: "NUM CHRONICALLY ABSENT",
+    field: "num_chronically_absent"
+  },
+  {
+    headerName: "PERCENT CHRONICALLY ABSENT",
+    field: "percent_chronically_absent"
+  },
+  {
+    headerName: "COMPLAINTS",
+    field: "complaints"
+  },
+  {
+    headerName: "MATERIAL INCIDENTS",
+    field: "material_incidents"
+  },
+  {
+    headerName: "LOCATION",
+    field: "location"
+  },
+  {
+    headerName: "POSTAL CODE",
+    field: "postal_code"
+  },
+  {
+    headerName: "COORDINATES",
+    field: "coordinates"
+  }
+];
+
